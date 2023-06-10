b64ac2d121153c888311ac048c93fb81d7d24fa8
mariko_mr <ma.kakiuchi@gmail.com>
2023-06-10 14:37:23 +0900

    会社情報の登録を作成

---
+++ part2/src/companies/create.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once __DIR__ . '/lib/mysqli.php';
+
+function createCompany($link, $company)
+{
+    $sql = <<< EOT
+    INSERT INTO companies(
+        name,
+        establishment_date,
+        founder
+        )VALUES(
+        "{$company['name']}",
+        "{$company['establishment_date']}",
+        "{$company['founder']}"
+    );
+EOT;
+
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        echo '登録が完了しました' . PHP_EOL;
+    } else {
+        echo 'Error:データの追加に失敗しました' . PHP_EOL;
+        echo 'Debugging error:' . mysqli_error($link) . PHP_EOL;
+    }
+}
+
+// HTTPメソッドがPOSTだったら
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // POSTされた会社情報を変数に格納する
+    $company = [
+        'name' => $_POST['name'],
+        'establishment_date' => $_POST['establishment_date'],
+        'founder' => $_POST['founder']
+    ];
+    // バリデーションする
+
+    // データベースに接続する
+    $link = dbConnect();
+    // データベースにデータを登録する
+    createCompany($link, $company);
+    // データベースとの接続を切断する
+    mysqli_close($link);
+}
+
+header("Location: index.php");
