e197621f98ad535824c62f490c42d89e35b98dc7
mariko_mr <ma.kakiuchi@gmail.com>
2023-06-15 20:39:47 +0900

    CSSの追加

--- part2/src/views/new.php
+++ part2/src/views/new.php
@@ -12,52 +12,69 @@
 </head>
 
 <body>
-    <h1>読書ログの登録</h1>
-    <?php if (!empty($errors)) : ?>
-        <ul>
-            <?php foreach ($errors as $error) : ?>
-                <li> <?php echo $error; ?></li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
+    <!-- header -->
+    <header class="Small shadow">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <h1>
+                <a class="text-decoration-none text-dark h2" href="index.php">読書ログ</a>
+            </h1>
+        </nav>
+    </header>
 
-    <form action="create.php" method="POST">
+    <div class="container">
+        <!-- error -->
+        <?php if (!empty($errors)) : ?>
+            <ul class="text-danger mt-5">
+                <?php foreach ($errors as $error) : ?>
+                    <li> <?php echo $error; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+
         <div>
-            <label for="title">書籍名</label>
-            <input type="text" id="title" name="title" value="<?php echo $book_log['title']; ?>">
+            <h2 class="h4">読書ログの登録</h2>
         </div>
-        <div>
-            <label for="author">著者名</label>
-            <input type="text" id="author" name="author" value="<?php echo $book_log['author']; ?>">
-        </div>
-        <div>
-            <!-- 下記をそれぞれ修正 -->
-            <label>読書状況</label>
-            <div>
-                <div>
-                    <input type="radio" id="status_unread" name="status" value="未読" <?php echo $book_log['status'] === '未読' ?  'checked' : '' ?>>
-                    <label for="status_unread">未読</label>
+
+        <!-- form -->
+        <div class="mt-4">
+            <form action="create.php" method="POST">
+                <div class="form-group">
+                    <label for="title">書籍名</label>
+                    <input type="text" id="title" name="title" class="form-control" value="<?php echo $book_log['title']; ?>">
                 </div>
-                <div>
-                    <input type="radio" id="status_inProgress" name="status" value="読んでいる" <?php echo $book_log['status'] === '読んでいる' ? 'checked' : '' ?>>
-                    <label for="status_inProgress">読んでいる</label>
+                <div class="form-group">
+                    <label for="author">著者名</label>
+                    <input type="text" id="author" name="author" class="form-control" value="<?php echo $book_log['author']; ?>">
                 </div>
-                <div>
-                    <input type="radio" id="status_completed" name="status" value="読了" <?php echo $book_log['status'] === '読了' ? 'checked' : '' ?>>
-                    <label for="status_completed">読了</label>
+                <div class="form-group">
+                    <label>読書状況</label>
+                    <div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" id="status_unread" name="status" value="未読" <?php echo $book_log['status'] === '未読' ?  'checked' : 'checked' ?>>
+                            <label class="form-check-label" for="status_unread">未読</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" id="status_inProgress" name="status" value="読んでいる" <?php echo $book_log['status'] === '読んでいる' ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="status_inProgress">読んでいる</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" id="status_completed" name="status" value="読了" <?php echo $book_log['status'] === '読了' ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="status_completed">読了</label>
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="rating">評価(5点満点の整数)</label>
+                    <input type="number" id="rating" name="rating" min="1" max="5" step="1" class="form-control" value="<?php echo $book_log['rating'] === 0 ? '' : $book_log['rating']; ?>">
+                </div>
+                <div class="form-group">
+                    <label for="review">感想</label>
+                    <textarea name="review" class="form-control" id="review"><?php echo $book_log['review']; ?></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">登録する</button>
+            </form>
         </div>
-        <div>
-            <label for="rating">評価(5点満点の整数)</label>
-            <input type="number" id="rating" name="rating" min="1" max="5" step="1" value="<?php echo $book_log['rating'] === 0 ? '' : $book_log['rating']; ?>">
-        </div>
-        <div>
-            <label for="review">感想</label>
-            <textarea name="review" id="review"><?php echo $book_log['review']; ?></textarea>
-        </div>
-        <button type="submit">登録する</button>
-    </form>
+    </div>
 </body>
 
 </html>
