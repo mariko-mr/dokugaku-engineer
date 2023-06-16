20a2ffbe1fb2db894aa363462279dd4f1cc8edac
mariko_mr <ma.kakiuchi@gmail.com>
2023-06-16 11:53:02 +0900

    XSS対策

--- part2/src/index.php
+++ part2/src/index.php
@@ -1,5 +1,32 @@
 <?php
 
+require_once __DIR__ . '/lib/mysqli.php';
+require_once __DIR__ . '/lib/escape.php';
+
+function listBookLog($link)
+{
+    $sql = <<< EOT
+ SELECT title, author , status , rating , review
+ FROM book_log;
+EOT;
+
+    // SQL文を実行して結果を取得
+    $results = mysqli_query($link, $sql);
+
+    $book_logs = [];
+    // 結果をそれぞれ$book_logsにいれる
+    while ($book_log = mysqli_fetch_assoc($results)) {
+        $book_logs[] = $book_log;
+    };
+
+    mysqli_free_result($results);
+
+    return $book_logs;
+}
+
+$link = dbConnect();
+$book_logs = listBookLog($link);
+
 $title = '読書ログの一覧';
 $content = __DIR__ . '/views/index.php';
 
--- part2/src/views/index.php
+++ part2/src/views/index.php
@@ -1,15 +1,25 @@
     <div>
         <h1>読書ログ</h1>
     </div>
-
     <div>
         <a href="new.php">読書ログを登録する</a>
     </div>
-
     <main>
-        <section>
-            <h2>新世界より</h2>
-            <div>貴志祐&nbsp;/&nbsp;読了&nbsp;/&nbsp;5点</div>
-            <div>おもしろかった</div>
-        </section>
+        <?php if (count($book_logs) > 0) : ?>
+            <?php foreach ($book_logs as $book_log) : ?>
+                <section>
+                    <h2><?php echo escape($book_log['title']); ?></h2>
+                    <div>
+                        <?php echo escape($book_log['author']); ?>&nbsp;/
+                        <?php echo escape($book_log['status']); ?>&nbsp;/
+                        <?php echo escape($book_log['rating']); ?>点
+                    </div>
+                    <div>
+                        <?php echo nl2br(escape($book_log['review']), false); ?>
+                    </div>
+                </section>
+            <?php endforeach; ?>
+        <?php else : ?>
+            <div>読書ログがありません。</div>
+        <?php endif; ?>
     </main>
