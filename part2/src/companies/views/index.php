c1e62a4caecbad9a0af2f0e2542f05f9cca695bd
mariko_mr <ma.kakiuchi@gmail.com>
2023-06-16 13:55:27 +0900

    index.phpへCSSの追加

--- part2/src/companies/views/index.php
+++ part2/src/companies/views/index.php
@@ -1,24 +1,26 @@
     <div>
         <h1 class="h2 text-dark mt-4">会社情報の一覧</h1>
     </div>
-    <div><a href="new.php">会社情報を登録する</a></div>
+    <div><a href="new.php" class="btn btn-primary mt-4">会社情報を登録する</a></div>
     <main>
 
         <?php if (count($companies) > 0) : ?>
 
             <?php foreach ($companies as $company) : ?>
-                <section>
-                    <h2><?php echo escape($company['name']); ?></h2>
-                    <div>創業&nbsp;:&nbsp;
-                        <?php echo escape($company['establishment_date']); ?>
-                        &nbsp;|&nbsp;代表&nbsp;:&nbsp;
-                        <?php echo escape($company['founder']); ?>
+                <section class="card shadow-sm mt-4">
+                    <div class="card-body">
+                        <h2 class="card-title h4"><?php echo escape($company['name']); ?></h2>
+                        <div>創業&nbsp;:&nbsp;
+                            <?php echo escape($company['establishment_date']); ?>
+                            &nbsp;|&nbsp;代表&nbsp;:&nbsp;
+                            <?php echo escape($company['founder']); ?>
+                        </div>
                     </div>
                 </section>
             <?php endforeach; ?>
 
         <?php else : ?>
-            <p>会社情報が登録されていません。</p>
+            <p class="mt-4">会社情報が登録されていません。</p>
         <?php endif; ?>
 
     </main>
--- part2/src/views/index.php
+++ part2/src/views/index.php
@@ -2,24 +2,26 @@
         <h1>読書ログ</h1>
     </div>
     <div>
-        <a href="new.php">読書ログを登録する</a>
+        <a href="new.php" class="btn btn-primary mt-4">読書ログを登録する</a>
     </div>
     <main>
         <?php if (count($book_logs) > 0) : ?>
             <?php foreach ($book_logs as $book_log) : ?>
-                <section>
-                    <h2><?php echo escape($book_log['title']); ?></h2>
-                    <div>
-                        <?php echo escape($book_log['author']); ?>&nbsp;/
-                        <?php echo escape($book_log['status']); ?>&nbsp;/
-                        <?php echo escape($book_log['rating']); ?>点
-                    </div>
-                    <div>
-                        <?php echo nl2br(escape($book_log['review']), false); ?>
+                <section class="card shadow-sm mt-4">
+                    <div class="card-body">
+                        <h2 class="card-title  h4"><?php echo escape($book_log['title']); ?></h2>
+                        <div class="small">
+                            <?php echo escape($book_log['author']); ?>&nbsp;/
+                            <?php echo escape($book_log['status']); ?>&nbsp;/
+                            <?php echo escape($book_log['rating']); ?>点
+                        </div>
+                        <div class="mt-2">
+                            <?php echo nl2br(escape($book_log['review']), false); ?>
+                        </div>
                     </div>
                 </section>
             <?php endforeach; ?>
         <?php else : ?>
-            <div>読書ログがありません。</div>
+            <div class="mt-4">読書ログがありません。</div>
         <?php endif; ?>
     </main>
