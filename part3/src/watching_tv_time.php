7c26a12c9113e51a625eead2bd3c69817d53a268
mariko_mr <ma.kakiuchi@gmail.com>
2023-06-22 21:31:04 +0900

    Create outputWatchingData()

--- part3/src/watching_tv_time.php
+++ part3/src/watching_tv_time.php
@@ -6,7 +6,6 @@ function createWatchingData($input)
     array_shift($input);
 
     $channelData = [];
-    //$channelData =[ 1=>[45, 2], 2=>[30, 1], 3=>[], 4=>[], 5=>[25, 1] ]
 
     for ($i = 0; $i < count($input); $i += 2) {
         $channel = (int)$input[$i];
@@ -17,62 +16,53 @@ function createWatchingData($input)
             $channelData[$channel]['minutes'] += $minutes;
             $channelData[$channel]['count']++;
         } else {
-            // 新しいチャンネル情報を登録
+            // チャンネルが無い場合は新しいチャンネル情報を登録
             $channelData[$channel] = [
                 'minutes' => $minutes,
-                // countを追加
                 'count' => 1,
             ];
         }
     }
-    /* ここを修正
-     * キーで昇順にソート
-     */
+
     ksort($channelData);
 
     return $channelData;
 }
 
-// 視聴時間を計算する
-/* ここを修正
- * 変数名を修正
- */
 function calWatchingHour($channelData)
 {
     $watchingMins = 0;
 
-    foreach ($channelData as $channel) {
-        $watchingMins += $channel['minutes'];
+    foreach ($channelData as $channel => $data) {
+        $watchingMins += $data['minutes'];
     }
 
     $watchingHours = round(($watchingMins / 60), 1);
     echo $watchingHours . PHP_EOL;
 }
 
-// 視聴channelと視聴時間をまとめる
+/* ここを修正
+ *
+ */
+// 視聴チャンネルと視聴時間と回数を出力
 function outputWatchingData($channelData)
 {
-    // foreach ($watchingData as $channelData) {
-    //     echo array_count_values($channelData);
-    // }
-
-
-    // 1~12chについて視聴時間と視聴回数を出力する
-    for ($channel = 1; $channel <= 12; $channel++) {
-        // 回数が0の場合は飛ばす
-
-        // 回数が1以上の場合は出力する
-        echo $channel . PHP_EOL;
+    // $channelData内全ての配列について出力
+    foreach ($channelData as $channel => $data) {
+        echo $channel . ' ' . $data['minutes'] . ' ' . $data['count'] . PHP_EOL;
     }
 }
 
 // コマンドラインの引き数から入力を取得
 $input = $argv;
+
 $channelData = createWatchingData($input);
 calWatchingHour($channelData);
-// outputWatchingData($watchingData);
+outputWatchingData($channelData);
 
-var_export($channelData);
+
+// 以下は全てあとで消す
+// var_export($channelData);
 
 // docker compose exec app php watching_tv_time.php 1 30 5 25 2 30 1 15
 
