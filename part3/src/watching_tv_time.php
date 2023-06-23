8d519939e2d3f2efaae897779bb6b74a540b14ad
mariko_mr <ma.kakiuchi@gmail.com>
2023-06-23 09:35:51 +0900

    バリデーション処理の追加

--- part3/src/watching_tv_time.php
+++ part3/src/watching_tv_time.php
@@ -1,5 +1,26 @@
 <?php
 
+// バリデーション処理する
+function validated($channel, $minutes)
+{
+    $validated = [];
+    // チャンネル
+    if ($channel < 1 || $channel > 12) {
+        $validated[] = 'チャンネルは1〜12の範囲(1ch〜12ch)で指定してください。';
+    }
+    // 視聴回数
+    if ($minutes < 1 || $minutes > 1440) {
+        $validated[] = '視聴分数は1〜1440の範囲で指定してください。';
+    }
+    // エラーがあればエラー文を出して処理を終了する
+    if (!empty($validated)) {
+        foreach ($validated as $error) {
+            echo $error . PHP_EOL;
+        }
+        exit;
+    }
+}
+
 function createWatchingData($input)
 {
     // スクリプト名を削除する
@@ -10,6 +31,8 @@ function createWatchingData($input)
     for ($i = 0; $i < count($input); $i += 2) {
         $channel = (int)$input[$i];
         $minutes = (int)$input[$i + 1];
+
+        validated($channel, $minutes);
 
         if (isset($channelData[$channel])) {
             // すでにチャンネルがある場合は視聴時間と回数を更新
@@ -54,3 +77,8 @@ $input = $argv;
 $channelData = createWatchingData($input);
 calWatchingHour($channelData);
 outputWatchingData($channelData);
+
+/* あとで消す
+ * docker compose exec app php watching_tv_time.php 1 30 5 25 2 30 1 15
+ * docker compose exec app php watching_tv_time.php 14 30 5 25 2 30 1 15
+ */
