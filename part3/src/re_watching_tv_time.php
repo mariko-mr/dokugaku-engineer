4db76e11dc3601ce7d848f3908de58f8037abab1
mariko_mr <ma.kakiuchi@gmail.com>
2023-06-28 12:41:51 +0900

    結果の出力を作成

--- part3/src/re_watching_tv_time.php
+++ part3/src/re_watching_tv_time.php
@@ -1,8 +1,5 @@
 <?php
 
-/* ここを修正
- * 定数MINUTES_PER_HOURの追加
- */
 const SPLIT_BY_TWO = 2;
 const MINUTES_PER_HOUR = 60;
 
@@ -14,58 +11,47 @@ function getInput()
     return array_chunk($inputs, SPLIT_BY_TWO);
 }
 
-function createViewingData(array $inputs): array
+function createViewingRecords(array $inputs): array
 {
-    $viewingData = [];  // [$channel => $togetherMinutes]
+    $viewingRecords = [];  // [$channel => $togetherMinutes]
 
     foreach ($inputs as $input) {
-
         $channel = $input[0]; // チャンネル番号
         $minutes = $input[1]; // 視聴分数
         $togetherMinutes = [$minutes];  // 視聴分数を格納する配列
 
-        if (array_key_exists($channel, $viewingData)) {
-            $togetherMinutes = [...$viewingData[$channel], ...$togetherMinutes];
+        if (array_key_exists($channel, $viewingRecords)) {
+            $togetherMinutes = [...$viewingRecords[$channel], ...$togetherMinutes];
         }
 
         // 視聴分数を更新
-        $viewingData[$channel] = $togetherMinutes;
+        $viewingRecords[$channel] = $togetherMinutes;
     }
 
-    ksort($viewingData);
-    return $viewingData;
+    ksort($viewingRecords);
+    return $viewingRecords;
+}
+
+function calTotalViewingHours($viewingRecords)
+{
+    $totalViewingTimes = array_sum(array_merge(...$viewingRecords));
+    return round(($totalViewingTimes / MINUTES_PER_HOUR), 1);
 }
 
 /* ここを修正
- * 視聴時間の計算を追加
+ * 結果の出力を追加
  */
-function calTotalViewingHours($viewingData)
+function display(array $viewingRecords)
 {
-    $totalViewingTimes = array_sum(array_merge(...$viewingData));
-    $totalViewingHours = round(($totalViewingTimes/MINUTES_PER_HOUR), 1);
+    $totalViewingHours = calTotalViewingHours($viewingRecords);
+    echo $totalViewingHours . PHP_EOL; // 1.7
 
-    return $totalViewingHours;
-}
-
-// 結果を出力する
-function display(array $viewingData)
-{
-    $totalViewingHours = calTotalViewingHours($viewingData);
-    echo $totalViewingHours;
+    foreach ($viewingRecords as $channel => $togetherMinutes) { // $togetherMinutesは配列
+        echo $channel . ' ' . array_sum($togetherMinutes) . ' ' . count($togetherMinutes) . PHP_EOL;  // 1 45 2
+    }
 }
 
 
 $inputs = getInput();
-$viewingData = createViewingData($inputs);
-display($viewingData);
-
-
-/* あとで消す
-docker compose exec app php re_watching_tv_time.php 1 30 5 25 2 30 1 15
-
- アウトプット例
- 1.7
- 1 45 2
- 2 30 1
- 5 25 1
- */
+$viewingRecords = createViewingRecords($inputs);
+display($viewingRecords);
