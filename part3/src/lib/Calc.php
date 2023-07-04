6d6b6b5de8fcb6dc05828b528fd93e854cf31c43
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-04 14:14:22 +0900

    Calc()の完成

--- part3/src/lib/Calc.php
+++ part3/src/lib/Calc.php
@@ -1,42 +1,91 @@
 <?php
 
-const TAX_RATE = 0.1;     // 消費税率10%
-const PRICES = [    // 商品番号 => 金額(税抜)
-    1 => 100,  // 玉ねぎ
-    2 => 150,  // にんじん
-    3 => 200,  // りんご
-    4 => 350,  // ぶどう
-    5 => 180,  // 牛乳
-    6 => 220,  // 玉子
-    7 => 440,  // 唐揚げ弁当
-    8 => 380,  // のり弁当
-    9 => 80,   // お茶
-    10 => 100  // コーヒー
+const ONION_COUNT_THREE = 3;
+const ONION_COUNT_FIVE = 5;
+const ONION_DISCOUNT_THREE = 50;
+const ONION_DISCOUNT_FIVE = 100;
+
+const SET_DISCOUNT = 20;
+const HALF_PRICE = 0.5;
+
+const TAX_RATE = 0.1;      // 消費税率10%
+const PRICES = [           // 商品番号 => []
+    1 => ['price' => 100, 'type' => ''],         // 玉ねぎ
+    2 => ['price' => 150, 'type' => ''],         // にんじん
+    3 => ['price' => 200, 'type' => ''],         // りんご
+    4 => ['price' => 350, 'type' => ''],         // ぶどう
+    5 => ['price' => 180, 'type' => 'drink'],    // 牛乳
+    6 => ['price' => 220, 'type' => ''],         // 玉子
+    7 => ['price' => 440, 'type' => 'lunchBox'], // 唐揚げ弁当
+    8 => ['price' => 380, 'type' => 'lunchBox'], // のり弁当
+    9 => ['price' => 80, 'type' => 'drink'],     // お茶
+    10 => ['price' => 100, 'type' => 'drink']    // コーヒー
 ];
 
-function calc(string $purchaseTime, array $productNumbers): int
+function calc(string $purchaseTime, array $productIds): int
 {
-    $taxIncludedPayment = 0;
-    // 先にディスカウントする
+    $taxIncludedPay = 0;
 
-
-    // 価格に税を含めたものを順番に足していくPRICES[番号]*(1+TAX_RATE)
-    foreach ($productNumbers as $productNumber) {
-        // お弁当は20〜23時はタイムセールで半額（$purchaseTimeが20〜23時 & 商品番号が7or8）
-        $taxIncludedPayment += PRICES[$productNumber] * (1 + TAX_RATE);
+    // 価格に税を含めたものを順番に足していく
+    foreach ($productIds as $productId) {
+        $taxIncludedPay += PRICES[$productId]['price'];
     }
 
-    return $taxIncludedPayment;
+    // ディスカウントする
+    $taxIncludedPay -= discountOnion($productIds);
+    $taxIncludedPay -= discountSet($productIds);
+    $taxIncludedPay -= discountLunchBox($purchaseTime, $productIds);
+
+    // ここで税込み計算をする
+    return floor($taxIncludedPay * (1 + TAX_RATE));
 }
 
-function getDiscount(string $purchaseTime, array $productNumbers)
+function discountOnion(array $productIds): int
 {
-    // 玉ねぎは5つ買うと100円引き（$productNumbersに1が5つ以上あると$taxIncludedPaymentから100円引く）
+    $count = array_count_values($productIds);
+    $onion = $count[1];
+    $discountOnion = 0;
 
-    // 玉ねぎは3つ買うと50円引き（$productNumbersに1が3つ以上あると$taxIncludedPaymentから50円引く
+    if ($onion >= ONION_COUNT_FIVE) {           // 玉ねぎは5つ買うと100円引き
+        $discountOnion += ONION_DISCOUNT_FIVE;
+    } elseif ($onion >= ONION_COUNT_THREE) {    // 玉ねぎは3つ買うと50円引き
+        $discountOnion += ONION_DISCOUNT_THREE;
+    }
 
-    // 弁当と飲み物を一緒に買うと20円引き（ただし適用は一組ずつ）
+    return $discountOnion;
+}
 
+// 弁当と飲み物を一緒に買うと20円引き（ただし適用は一組ずつ）
+function discountSet(array $productIds): int
+{
+    // ドリンクの数、弁当の数を取得
+    $count = array_count_values($productIds);
+    $lunchBox = $count[7] + $count[8];
+    $drink = $count[5] + $count[9] + $count[10];
+
+    $discountSet = 0;
+
+    for ($i = 0; $i < $lunchBox && $i < $drink; $i++) {
+        $discountSet += SET_DISCOUNT;
+    }
+
+    return $discountSet;
+}
+
+// お弁当は20〜23時はタイムセールで半額
+function discountLunchBox(string $purchaseTime, array $productIds): int
+{
+    $discountLunchBox = 0;
+
+    if (date('20:00') <= $purchaseTime || $purchaseTime >= date('23:00')) {
+        foreach ($productIds as $productId) {
+            if (PRICES[$productId]['type'] === 'lunchBox') {
+                $discountLunchBox += PRICES[$productId]['price'] * HALF_PRICE;
+            }
+        }
+    }
+
+    return $discountLunchBox;
 }
 
 calc('21:00', [1, 1, 1, 3, 5, 7, 8, 9, 10]);
