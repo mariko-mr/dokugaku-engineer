e793190a7a396ce9b5f12ad9a4352525322b930a
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-04 15:39:22 +0900

    echoを追加

--- part3/src/total_payment.php
+++ part3/src/total_payment.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once (__DIR__ . '/lib/Calc.php');
+require_once(__DIR__ . '/lib/Calc.php');
 
-calc('21:00', [1, 1, 1, 3, 5, 7, 8, 9, 10]);
+echo calc('21:00', [1, 1, 1, 3, 5, 7, 8, 9, 10]);
