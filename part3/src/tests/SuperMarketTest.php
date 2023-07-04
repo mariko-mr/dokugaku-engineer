b314cea6796625c1f84fafd2b9fd1f06a1cce34f
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-04 21:46:27 +0900

    $countの定義をcalc()へ移動

---
+++ part3/src/tests/SuperMarketTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require_once(__DIR__ . '/../lib/SuperMarket.php');
+final class SuperMarketTest extends TestCase
+{
+    public function testCalc(): void
+    {
+        $this->assertSame(1298, calc('21:00', [1, 1, 1, 3, 5, 7, 8, 9, 10]));
+    }
+}
