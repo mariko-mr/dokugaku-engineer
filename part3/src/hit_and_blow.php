6b3cf5d35a029dac0d8e2f6c5f208df186b639ce
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-06 14:19:36 +0900

    新規作成

---
+++ part3/src/hit_and_blow.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once(__DIR__ . '/lib/HitAndBlow.php');
+
+print_r(judge(5678, 5679));
---
+++ part3/src/lib/HitAndBlow.php
@@ -0,0 +1,35 @@
+<?php
+
+// 引き数を扱いやすく配列にする
+function getArray(int $numbers): array
+{
+    return str_split($numbers);
+}
+
+function judge(int $correctNumbers, int $userAnswers): array
+{
+    $correctNumbers = getArray($correctNumbers);
+    $userAnswers = getArray($userAnswers);
+
+    $hit = 0;
+    $blow = 0;
+
+    for ($i = 0; $i < 4; $i++) {
+        if ($userAnswers[$i] === $correctNumbers[$i]) {
+            // $userAnswersのi番目が$correctNumbersのi番目と同じなら$hit
+            $hit++;
+        } elseif (in_array($userAnswers[$i], $correctNumbers, true)) {
+            // $userAnswersのi番目が$correctNumbersに含まれるなら$blow
+            $blow++;
+        }
+    }
+
+    return [$hit, $blow];
+}
+
+/*
+◯実行例($correctNumbers, $userAnswers)
+judge(5678, 5678) //=> [4, 0]
+judge(5678, 7612) //=> [1, 1]
+judge(5678, 8756) //=> [0, 4]
+*/
