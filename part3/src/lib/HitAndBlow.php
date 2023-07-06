ffce22e96aac88a3915c1213de7f9552bc3bfa5e
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-06 14:33:32 +0900

    型の宣言を追加

--- part3/src/lib/HitAndBlow.php
+++ part3/src/lib/HitAndBlow.php
@@ -1,11 +1,20 @@
 <?php
 
+/* ここを修正
+ * str_split()で$numbersをstringに変換
+ */
+/**
+ * @return array<int,string>
+ */
 // 引き数を扱いやすく配列にする
 function getArray(int $numbers): array
 {
-    return str_split($numbers);
+    return str_split((string)$numbers);
 }
 
+/**
+ * @return array<int,int>
+ */
 function judge(int $correctNumbers, int $userAnswers): array
 {
     $correctNumbers = getArray($correctNumbers);
