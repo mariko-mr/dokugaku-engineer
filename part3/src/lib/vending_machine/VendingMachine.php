3828c2e2e67b6a6d45490785dad4855dbb6d913d
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-14 15:48:06 +0900

    クイズ: 自動販売機にアクセス権を付けよう

--- part3/src/lib/vending_machine/VendingMachine.php
+++ part3/src/lib/vending_machine/VendingMachine.php
@@ -1,9 +1,23 @@
 <?php
-
 class VendingMachine
 {
+    private const DRINK_PRICE = 100;
+    private int $depositedCoin = 0 ;
+
+    public function depositCoin(int $coin): int
+    {
+        if ($coin === self::DRINK_PRICE) {
+            $this->depositedCoin = $coin;
+        }
+        return $this->depositedCoin;
+    }
+
     public function pressButton(): string
     {
-        return 'cider';
+        if ($this->depositedCoin === self::DRINK_PRICE) {
+            return 'cider';
+        }
+
+        return '';
     }
 }
--- part3/src/tests/vending_machine/VendingMachineTest.php
+++ part3/src/tests/vending_machine/VendingMachineTest.php
@@ -8,10 +8,24 @@ require_once(__DIR__ . '/../../lib/vending_machine/VendingMachine.php');
 
 final class VendingMachineTest extends TestCase
 {
+    public function testDepositCoin()
+    {
+        $vendingMachine = new VendingMachine();
+        $this->assertSame(0, $vendingMachine->depositCoin(0));
+        $this->assertSame(0, $vendingMachine->depositCoin(150));
+        $this->assertSame(100, $vendingMachine->depositCoin(100));
+    }
+
     public function testPressButton()
     {
         $vendingMachine = new VendingMachine();
+
+        $this->assertSame('', $vendingMachine->pressButton());
+
+        // 100円を入れた後の状態をテストするため、100円を追加する
+        $vendingMachine->depositCoin(100);
         $this->assertSame('cider', $vendingMachine->pressButton());
+
     }
 }
 
