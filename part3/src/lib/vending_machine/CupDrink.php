fa3324a9ba2c4d28ad8d5d6cc527c07f9e36f2d5
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-20 11:57:06 +0900

    定数名をPRICEへ変更

--- part3/src/lib/vending_machine/CupDrink.php
+++ part3/src/lib/vending_machine/CupDrink.php
@@ -1,16 +1,17 @@
 <?php
 
 require_once(__DIR__ . '/../../lib/vending_machine/Item.php');
+
 class CupDrink extends Item
 {
-    private const DRINK = [
+    private const PRICE = [
         'hot cup coffee' => 100,
         'ice cup coffee' => 100,
     ];
 
     public function getPrice(): int
     {
-        return self::DRINK[$this->name];
+        return self::PRICE[$this->name];
     }
 
     public function getCupNumber(): int
--- part3/src/lib/vending_machine/Drink.php
+++ part3/src/lib/vending_machine/Drink.php
@@ -1,16 +1,17 @@
 <?php
 
 require_once(__DIR__ . '/../../lib/vending_machine/Item.php');
+
 class Drink extends Item
 {
-    private const DRINK = [
+    private const PRICE = [
         'cider' => 100,
         'coke' => 150,
     ];
 
     public function getPrice(): int
     {
-        return self::DRINK[$this->name];
+        return self::PRICE[$this->name];
     }
 
     public function getCupNumber(): int
