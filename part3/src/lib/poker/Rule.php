5b872d06599571596d5045772c99aefbf111ec3e
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-20 10:50:21 +0900

    インターフェイスに変更

--- part3/src/lib/poker/Rule.php
+++ part3/src/lib/poker/Rule.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once(__DIR__ . '/../../lib/poker/Rule.php');
-
-abstract class Rule
+interface Rule
 {
-    abstract public function getHand(array $pokerCards);
+    public function getHand(array $pokerCards);
 }
