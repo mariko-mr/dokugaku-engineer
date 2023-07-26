eb6274bc4cd7cacff7fa613efab237c4efea7c14
mariko_mr <ma.kakiuchi@gmail.com>
2023-07-26 22:09:47 +0900

    ファイル名の変更

--- part3/src/lib/vending_machine/DepositItem.php
+++ part3/src/lib/vending_machine/DepositItem.php
@@ -6,5 +6,5 @@ require_once(__DIR__ . '/../../lib/vending_machine/Item.php');
 
 abstract class DepositItem extends Item
 {
-    abstract public function getMaxDeposit();
+    abstract public function depositItem(int $stock);
 }
